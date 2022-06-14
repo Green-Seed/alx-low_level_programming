d021ffabc050a84fef5ac4d76d9f2e4a32261d1b
Green-Seed <ngubanethandiwe134@gmail.com>
2022-06-13 21:00:06 -0700

    Task 0 to 9

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  * _strlen - function that returns the length of a string
+ *   * @s: length of a string
+ *    *Return: 0
+ *     */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (; *s != '\0'; s++)
+	{
+		len += 1;
+	}
+	return (len);
+}	
