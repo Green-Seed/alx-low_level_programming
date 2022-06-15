cd2a0916854e45866de6626deb763fb9e94a382c
Green-Seed <ngubanethandiwe134@gmail.com>
2022-06-14 18:49:28 -0700

    Length

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,10 @@
 #include ""main.h"
-/**swap_int - swap the value of two integers 
- * @a: first integer
- * @b: second integer
- *Return: void
- */
+/**
+* swap_int - swap the value of two integers
+* @a: first integer
+* @b: second integer
+* Return: void
+*/
 void swap_int(int *a, int *b)
 {
 	int c;
