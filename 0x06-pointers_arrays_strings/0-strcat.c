0ad99cb4c8b5aad102db1368ca4ebe8c0a65d12d
Green-Seed <ngubanethandiwe134@gmail.com>
2022-06-16 15:00:57 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/** strcat - concatenates two strings
+ * @str - string to be changed
+ * @src - string to be changed
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
+
