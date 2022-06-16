1d7d2800be94b8bf8e0dc9f1df11e45075e00b95
Green-Seed <ngubanethandiwe134@gmail.com>
2022-06-16 07:18:18 -0700

    Tasks

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,14 @@
 
 void puts2(char *str)
 {
-	int len, i;
+	int i;
 
-	len = strlen(str);
-	for (i + 0; i < len; += 2)
-		_putchar(str[i]);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2; == 0)
+			_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
 }
